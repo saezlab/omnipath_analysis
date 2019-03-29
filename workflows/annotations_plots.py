75dbee78898bf91b149fb6cb1af9bbd7ed0cc2aa
Nico <nicolaspalacio91@gmail.com>
2019-03-29 19:21:46 +0100

    Started annotation_plots (WIP)

---
+++ workflows/annotations_plots.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Nicolàs Palacio-Escat
+# nicolas.palacio@bioquant.uni-heidelberg.de
+
+import os
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+import pypath
+from pypath import annot
+
+# Colors!
+cg = (87/255, 171/255, 39/255)
+cb = (0/255, 84/255, 159/255)
+
+if os.getcwd().endswith('omnipath2'):
+    os.chdir('workflows')
+
+if not os.path.exists('new_cache'):
+    os.makedirs('new_cache')
+
+pypath.settings.setup(cachedir='new_cache')
+
+a = annot.AnnotationTable(keep_annotators=True)
+a.load()
+
+
+a.names[1]
+for i in a.names:
+    try:
+        print('__'.join(i))
+    except:
+        print('#######################')
+        print(i)
+        print('#######################')
+
+
+
+['__'.join(name) for name in a.names]
+dir(a)
+pd.DataFrame(a.data, index=a.uniprots, columns=a.names) ==
+a.to_dataframe()
