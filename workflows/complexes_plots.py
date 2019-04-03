a7bca7784189076892ac2424a7491e8c2b20fcb1
Nico <nicolaspalacio91@gmail.com>
2019-04-03 10:22:54 +0200

    Updated complexes_plots
    Horizontal barplots and colors

--- workflows/complexes_plots.py
+++ workflows/complexes_plots.py
@@ -13,8 +13,8 @@ import pypath
 from pypath import complex
 
 # Colors!
-cg = (87/255, 171/255, 39/255)
-cb = (0/255, 84/255, 159/255)
+green = (87/255, 171/255, 39/255)
+blue = (0/255, 84/255, 159/255)
 
 cachedir = '/home/nico/pypath_cache'
 
@@ -45,7 +45,6 @@ for k, v in co.complexes.items():
     unique_sources.update(v.sources)
     unique_refs.update(v.references)
     aux = k.split('-')
-    unique_prots.update(aux)
 
     if len(aux) == 1:
         homomultimer += 1
@@ -53,18 +52,18 @@ for k, v in co.complexes.items():
     else:
         heteromultimer += 1
 
+homopt = 100 * homomultimer / total
+heteropt = 100 * heteromultimer / total
 # Unique proteins across all complexes
 print('Total number of unique proteins within complexes:', len(co.proteins))
 print('Out of %d complexes, %d are homomultimers (%.2f %%) and %d '
-      'heteromultimers (%.2f %%)' % (total, homomultimer,
-                                     100 * homomultimer / total,
-                                     heteromultimer,
-                                     100 * heteromultimer / total))
+      'heteromultimers (%.2f %%)' % (total, homomultimer, homopt,
+                                     heteromultimer, heteropt))
 fig, ax = plt.subplots()
 ax.pie([homomultimer, heteromultimer],
-       labels=['Homomultimers', 'Heteromultimers'],
-       autopct='%.2f %%',
-       colors=[cg, cb])
+       labels=['Homomultimers\n%.2f %%' % homopt,
+               'Heteromultimers\n%.2f %%' % heteropt],
+       colors=[green, blue])
 ax.set_title('Complex types')
 fig.tight_layout()
 fig.savefig('../figures/complex_types.svg')
@@ -79,13 +78,13 @@ for c in co.complexes.values():
     for s in c.sources:
         comp_by_res[s] += 1
 
-comp_by_res = pd.Series(comp_by_res).sort_values(ascending=False)
+comp_by_res = pd.Series(comp_by_res).sort_values(ascending=True)
 rng = range(len(comp_by_res))
 fig, ax = plt.subplots()
-ax.bar(rng, comp_by_res.values, color=cb)
-ax.set_xticks(rng)
-ax.set_xticklabels(comp_by_res.index, rotation=90)
-ax.set_xlabel('Resource')
-ax.set_ylabel('Number of complexes')
+ax.barh(rng, comp_by_res.values, color=blue)
+ax.set_yticks(rng)
+ax.set_yticklabels(comp_by_res.index)
+ax.set_ylabel('Resource')
+ax.set_xlabel('Number of complexes')
 fig.tight_layout()
 fig.savefig('../figures/complex_by_source.svg')
