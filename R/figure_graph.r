bdc1ba235374c2f9148ab9051801bcf200ab7380
Türei Dénes <turei.denes@gmail.com>
2019-07-08 15:20:24 +0200

    graph plot classes

--- R/figure_graph.r
+++ R/figure_graph.r
@@ -26,54 +26,105 @@ require(tidygraph)
 require(R6)
 
 
-if(FALSE){
+GraphPlot <- R6::R6Class(
     
-    make_fr_layout <- function(g){
-        # layout with qgraph
-        # g is an igraph object
-        el <- get.edgelist(g, names = FALSE)
-        lo <- qgraph.layout.fruchtermanreingold(el, vcount = vcount(g),
-                                                area = vcount(g)^2.3,
-                                                repulse.rad = vcount(g)^2.1,
-                                                niter = 3000)
-        return(lo)
-    }
+    'GraphPlot',
+    
+    inherit = SinglePlot,
+    
+    lock_objects = FALSE,
+    
+    public = list(
+        
+        initialize = function(...){
+            
+            super$initialize(...)
+            
+        },
+        
+        
+        set_fr_layout = function(){
+            
+            self$layout <- self$fr_layout(self$graph)
+            
+        },
+        
+        
+        #' Layout with `qgraph`.
+        #' `g` is an `igraph` object.
+        fr_layout = function(g){
+            
+            el <- get.edgelist(g, names = FALSE)
+            lo <- qgraph.layout.fruchtermanreingold(
+                el,
+                vcount = vcount(g),
+                area = vcount(g)^2.3,
+                repulse.rad = vcount(g)^2.1,
+                niter = 3000
+            )
+            
+            return(lo)
+            
+        }
+        
+    )
+    
+)
 
-    edges <- d %>%
-        filter(typ_cls0 == 'main' & typ_cls1 == 'main') %>%
-        select(name_cls0, name_cls1, con_all, size_cls0, size_cls1) %>%
-        filter(size_cls0 > 0 & size_cls1 > 0)
 
-    vertices <- bind_rows(
-            edges %>% select(name = name_cls0, size = size_cls0),
-            edges %>% select(name = name_cls1, size = size_cls1)
-        ) %>%
-        group_by(name) %>%
-        summarize_all(first)
+ConnectionGraph <- R6::R6Class(
+    
+    'ConnectionGraph',
+    
+    inherit = GraphPlot,
+    
+    lock_objects = FALSE,
+    
+    public = list(
+        
+        initialize = function(...){
+            
+            
 
-    edges <- edges %>% select(name_cls0, name_cls1, con_all)
+            edges <- d %>%
+                filter(typ_cls0 == 'main' & typ_cls1 == 'main') %>%
+                select(name_cls0, name_cls1, con_all, size_cls0, size_cls1) %>%
+                filter(size_cls0 > 0 & size_cls1 > 0)
 
-    g <- graph_from_data_frame(edges, vertices = vertices, directed = FALSE)
-    V(g)$size <- V(g)$size / max(V(g)$size) * 50 + 30
-    E(g)$width <- E(g)$con_all / max(E(g)$con_all) * 20 + 3
+            vertices <- bind_rows(
+                    edges %>% select(name = name_cls0, size = size_cls0),
+                    edges %>% select(name = name_cls1, size = size_cls1)
+                ) %>%
+                group_by(name) %>%
+                summarize_all(first)
 
-    g <- simplify(g, edge.attr.comb = first, remove.loops = FALSE)
+            edges <- edges %>% select(name_cls0, name_cls1, con_all)
 
-    lo <- make_fr_layout(g)
+            g <- graph_from_data_frame(edges, vertices = vertices, directed = FALSE)
+            V(g)$size <- V(g)$size / max(V(g)$size) * 50 + 30
+            E(g)$width <- E(g)$con_all / max(E(g)$con_all) * 20 + 3
 
-    cairo_pdf('figures/intercell_graph.pdf', height = 5, width = 5)
+            g <- simplify(g, edge.attr.comb = first, remove.loops = FALSE)
 
-        plot(
-            g,
-            layout = lo,
-            vertex.frame.color = NA,
-            vertex.color = '#FDC70F',
-            edge.color = '#4C4B6B33',
-            vertex.label.family = 'DINPro',
-            vertex.label.color = '#4C4B6B',
-            edge.width = E(g)$width
-        )
+            lo <- make_fr_layout(g)
 
-    dev.off()
+            cairo_pdf('figures/intercell_graph.pdf', height = 5, width = 5)
 
-}
+                plot(
+                    g,
+                    layout = lo,
+                    vertex.frame.color = NA,
+                    vertex.color = '#FDC70F',
+                    edge.color = '#4C4B6B33',
+                    vertex.label.family = 'DINPro',
+                    vertex.label.color = '#4C4B6B',
+                    edge.width = E(g)$width
+                )
+
+            dev.off()
+        
+        }
+        
+    )
+    
+)
