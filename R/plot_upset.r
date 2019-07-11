7ac32d5e27109d9f037633ffbdab6c646fcd0633
Türei Dénes <turei.denes@gmail.com>
2019-07-11 04:21:45 +0200

    upset plot for complexes

---
+++ R/plot_upset.r
@@ -0,0 +1,93 @@
+#!/usr/bin/env Rscript
+
+#
+#  This file is part of the `omnipath2` R package
+#
+#  Copyright
+#  2019
+#  Heidelberg University, Uniklinik RWTH Aachen
+#
+#  File author(s): Dénes Türei (turei.denes@gmail.com)
+#                  Nicolàs Palacio
+#
+#  Distributed under the GPLv3 License.
+#  See accompanying file LICENSE.txt or copy at
+#      http://www.gnu.org/licenses/gpl-3.0.html
+#
+#  Website: http://pypath.omnipathdb.org/
+#
+
+
+require(R6)
+
+
+UpsetComplexes <- R6::R6Class(
+    
+    'UpsetComplexes',
+    
+    inherit = UpsetBase,
+    
+    lock_objects = FALSE,
+    
+    public = list(
+        
+        initialize = function(
+                data = NULL,
+                plot_args = list(),
+                ...
+            ){
+            
+            self$data <- data
+            private$ensure_data()
+            
+            plot_args <- modifyList(
+                list(
+                    #title = 'Complexes by resource',
+                    order.by = 'freq',
+                    show.numbers = 'no',
+                    mb.ratio = c(.55, .45),
+                    scale.intersections = 'log10',
+                    text.scale = 1.5
+                ),
+                plot_args
+            )
+            
+            super$initialize(
+                ent_col = complex_id,
+                cat_col = resource,
+                name = 'complexes-by-resource',
+                plot_args = plot_args,
+                width = 7,
+                height = 4,
+                ...
+            )
+            
+            invisible(self)
+            
+        }
+        
+    ),
+    
+    
+    private = list(
+        
+        ensure_data = function(){
+            
+            if(is.null(self$data)){
+                
+                private$read_data()
+                
+            }
+            
+        },
+        
+        
+        read_data = function(){
+            
+            self$data <- ComplexesByResource$new()$data
+            
+        }
+        
+    )
+    
+)
---
+++ R/plot_upset_base.r
@@ -0,0 +1,145 @@
+#!/usr/bin/env Rscript
+
+#
+#  This file is part of the `omnipath2` R package
+#
+#  Copyright
+#  2019
+#  Heidelberg University, Uniklinik RWTH Aachen
+#
+#  File author(s): Dénes Türei (turei.denes@gmail.com)
+#                  Nicolàs Palacio
+#
+#  Distributed under the GPLv3 License.
+#  See accompanying file LICENSE.txt or copy at
+#      http://www.gnu.org/licenses/gpl-3.0.html
+#
+#  Website: http://pypath.omnipathdb.org/
+#
+
+require(dplyr)
+require(UpSetR)
+require(grid)
+require(R6)
+
+
+UpsetBase <- R6::R6Class(
+    
+    'UpsetBase',
+    
+    inherit = SinglePlot,
+    
+    lock_objects = FALSE,
+    
+    public = list(
+        
+        initialize = function(
+                name,
+                cat_col,
+                ent_col,
+                #title = NULL,
+                data = NULL,
+                ...
+            ){
+            
+            self$data <- `if`(is.null(self$data), data, self$data)
+            self$cat_col <- enquo(cat_col)
+            self$ent_col <- enquo(ent_col)
+            #self$title <- title
+            
+            super$initialize(data = data, name = name, ...)
+            
+            invisible(self)
+            
+        },
+        
+        
+        plot = function(...){
+            
+            self$plt <- do.call(
+                upset,
+                c(
+                    list(
+                        fromList(self$categories)
+                    ),
+                    list(...)
+                ),
+                
+            )
+            
+            invisible(self)
+            
+        }
+        
+    ),
+    
+    
+    private = list(
+        
+        setup = function(){
+            
+            private$ensure_data()
+            private$collect_categories()
+            
+        },
+        
+        
+        ensure_data = function(){
+            
+            if(is.null(self$data)){
+                
+                stop('omnipath2::UpsetBase: No data provided.')
+                
+            }
+            
+        },
+        
+        
+        collect_categories = function(){
+            
+            self$names <- self$data %>%
+                select(!!self$cat_col) %>%
+                unlist() %>%
+                unique()
+            
+            self$categories <- setNames(
+                sapply(
+                    self$names,
+                    function(name){
+                        self$data %>%
+                            filter(!!self$cat_col == name) %>%
+                            select(!!self$ent_col) %>%
+                            unlist() %>%
+                            unique()
+                    }
+                ),
+                self$names
+            )
+            
+            if(is.null(self$plot_args$nsets)){
+                
+                self$plot_args$nsets <- length(self$names)
+                
+            }
+            
+        },
+        
+        
+        post_plot = function(){
+            
+            if(!is.null(self$title)){
+                
+                grid.text(
+                    self$title,
+                    x = 0.65,
+                    y = 0.95,
+                    gp = gpar(fontsize = 20)
+                )
+                
+            }
+            
+        }
+        
+    )
+    
+)
