5eec8fab717ac8ae701666bbbb30ef4710a6e001
Türei Dénes <turei.denes@gmail.com>
2019-08-14 01:45:46 +0200

    little theme adjustments

---
+++ R/theme.r
@@ -0,0 +1,34 @@
+#!/usr/bin/env Rscript
+
+#
+#  This file is part of the `omnipath2` R package
+#
+#  Copyright
+#  2019
+#  Heidelberg University, Uniklinik RWTH Aachen
+#
+#  File author(s): Dénes Türei (turei.denes@gmail.com)
+#                  Nicolàs Palacio
+#
+#  Distributed under the GPLv3 License.
+#  See accompanying file LICENSE.txt or copy at
+#      http://www.gnu.org/licenses/gpl-3.0.html
+#
+#  Website: http://pypath.omnipathdb.org/
+#
+
+require(ggplot2)
+
+
+x_vertical_labels <- function(){
+    
+    list(
+        axis.text.x = element_text(
+            angle = 90,
+            vjust = .5,
+            hjust = 1,
+            color = '#000000'
+        )
+    )
+    
+}
