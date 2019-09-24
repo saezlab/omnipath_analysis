0b1099d6df2349b14969098e92034520a5a0d6fc
Türei Dénes <turei.denes@gmail.com>
2019-09-24 14:49:45 +0200

    compile_data: Database class generic methods for load & build

--- workflows/compile_data.py
+++ workflows/compile_data.py
@@ -80,6 +80,12 @@ defaults = {
     'tf_mirna_mod': 'main',
     'mirna_mrna_mod': 'main',
     
+    'omnipath_args': {
+        'use_omnipath': True,
+        'kinase_substrate_extra': True,
+        'ligand_receptor_extra': True,
+        'pathway_extra': True,
+    },
 }
 
 
@@ -161,13 +167,21 @@ class Database(session_mod.Logger)
         
         return os.path.join(
             self.pickle_dir,
-            etattr(self, '%s_pickle' % dataset),
+            getattr(self, '%s_pickle' % dataset),
         )
     
     
     def pickle_exists(self, dataset):
         
         return os.path.exists(self.pickle_path(dataset))
+    
+    
+    def table_path(self, dataset):
+        
+        return os.path.join(
+            self.tables_dir,
+            getattr(self, '%s_tsv' % dataset),
+        )
     
     
     def build_dataset(self, dataset):
@@ -208,9 +222,9 @@ class Database(session_mod.Logger)
             {}
         )
         
-        if hasattr(self, 'get_%s_args'):
+        if hasattr(self, 'get_args_%s' % dataset):
             
-            args.update(getattr(self, 'get_%s_args' % dataset)())
+            args.update(getattr(self, 'get_args_%s' % dataset)())
         
         return args
     
@@ -234,154 +248,67 @@ class Database(session_mod.Logger)
         return time.strftime('%Y%m%d')
     
     
-    def build_network_complete(self):
+    def get_args_curated(self):
         
-        main.init_db(
-            use_omnipath = True,
-            kinase_substrate_extra = True,
-            ligand_receptor_extra = True,
-            pathway_extra = True,
-        )
-        main.db.save_network(pfile = self.omnipath_pickle)
-    
-    
-    def build_network_curated(self):
-        
-        curated = main.PyPath()
         resources = copy.deepcopy(data_formats.pathway)
         resources.update(copy.deepcopy(data_formats.enzyme_substrate))
-        curated.init_network(resources)
-        curated.save_network(pfile = self.curated)
-    
-    
-    def build_complex(self):
         
-        co = complex.get_db()
-        co.save_to_pickle(self.complex_pickle)
+        return {'lst': resources}
     
     
-    def build_annot(self):
-        
-        a = annot.get_db()
-        a.save_to_pickle(annotation_pickle)
-    
-    
-    def build_intercell(self):
-        
-        i = intercell.IntercellAnnotation()
-        i.save_to_pickle(self.intercell_pickle)
-    
-    
-    def build_enzyme_substrate(self):
-        
-        m = ptm.get_db()
-        m.save_to_pickle(self.enz_sub_pickle)
-    
-    
-    def build_transcription(self):
+    def get_args_tf_target(self):
         
         transcription = copy.deepcopy(data_formats.transcription)
         transcription['tfregulons'].input_args = {
             'levels': self.tfregulons_levels,
         }
         
-        tr = main.PyPath()
-        tr.init_network(transcription)
-        tr.save_network(pfile = self.tfregulons_pickle)
+        return {'lst': transcription}
     
     
-    def build_tf_mirna(self):
+    def get_args_tf_mirna(self):
         
-        tfm = main.PyPath()
-        tfm.init_network(data_formats.tf_mirna)
-        tfm.save_network(pfile = self.tfmirna_pickle)
+        return {'lst': data_formats.tf_mirna}
     
     
-    def build_mirna_target(self):
+    def get_args_mirna_mrna(self):
         
-        mit = main.PyPath()
-        mit.init_network(data_formats.mirna_target)
-        mit.save_network(pfile = self.mirna_mrna_pickle)
-    
-    
-    def load(self):
-        
-        self.cplex = complex.get_db(pickle_file = self.complex_pickle)
-        self.annot = annot.get_db(pickle_file = self.annotations_pickle)
-        self.intercell = intercell.get_db(pickle_file = self.intercell_pickle)
-        self.omnipath = main.get_db(pfile = self.omnipath_pickle)
-        self.curated = main.PyPath()
-        self.curated.init_network(pfile = self.curated_pickle)
-        self.enz_sub = ptm.get_db(pickle_file = self.enz_sub_pickle)
-        self.trascr = main.PyPath()
-        self.transcr.init_network(pfile = self.tfregulons_pickle)
-        self.tf_mirna = main.PyPath()
-        self.tf_mirna.init_network(pfile = self.tfmirna_pickle)
-        self.mirna_mrna = main.PyPath()
-        self.mirna_mrna.init_network(pfile = self.mirna_mrna_pickle)
+        return {'lst': data_formats.mirna_target}
     
     
     def compile_tables(self):
         
+        for dataset in self.datasets:
+            
+            self.compile_table(dataset)
+    
+    
+    def compile_table(self, dataset):
         
-        self.omnipath.update_summaries()
-        self.omnipath.summaries_tab(outfile = self.network_complete_tsv)
+        table_path = self.table_path(dataset)
+        db = self.get_db(dataset)
+        db.update_summaries()
+        db.summaries_tab(outfile = table_path)
+    
+    
+    def get_db(self, dataset):
         
-        self.omnipath.update_summaries()
-        self.omnipath.summaries_tab(outfile = self.network_curated_tsv)
-        
-        self.transcr.update_summaries()
-        self.transcr.summaries_tab(outfile = self.network_tf_tsv)
-        
-        self.tf_mirna.update_summaries()
-        self.tf_mirna.summaries_tab(outfile = self.network_tf_mirna_tsv)
-        
-        self.mirna_mrna.update_summaries()
-        self.mirna_mrna.summaries_tab(outfile = self.network_mirna_target_tsv)
-        
-        self.cplex.update_summaries()
-        self.cplex.summaries_tab(self.complex_tsv)
-        
-        self.annot.update_summaries()
-        self.annot.summaries_tab(outfile = self.annotations_tsv)
-        
-        self.intercell.update_summaries()
-        self.summaries_tab(outfile = self.intercell_tsv)
+        return getattr(self, dataset)
 
-# count annotation records
+#
+# to be removed once we have it elsewhere:
+#
 
-# proteins
-sum([aaa.numof_protein_records() for aaa in a.annots.values()]) # 1972327
+## count annotation records
 
-# complexes
-sum([aaa.numof_complex_records() for aaa in a.annots.values()]) # 218360
+## proteins
+#sum([aaa.numof_protein_records() for aaa in a.annots.values()]) # 1972327
 
-# miRNAs
-sum([aaa.numof_mirna_records() for aaa in a.annots.values()]) # 355
+## complexes
+#sum([aaa.numof_complex_records() for aaa in a.annots.values()]) # 218360
 
-# total
-sum([aaa.numof_records() for aaa in a.annots.values()]) # 2191042
+## miRNAs
+#sum([aaa.numof_mirna_records() for aaa in a.annots.values()]) # 355
 
-# count annotated entities
-
-all_annotated_proteins = set.union(
-    *(set(aaa.all_proteins()) for aaa in a.annots.values())
-)
-
-all_uniprots = set(uniprot_input.all_uniprots(swissprot = 'yes'))
-
-len(all_annotated_proteins & all_uniprots) # 20066
-
-# number of annotated complexes
-all_annotated_complexes = set.union(
-    *(set(aaa.all_complexes()) for aaa in a.annots.values())
-)
-
-len(all_annotated_complexes) # 16477
-
-# number of complexes in OmniPath:
-all_complexes = set(complex.db.complexes.values())
-len(all_complexes)
-
-
-cplex_stats = c.summaries_tab(outfile = 'complex_stats.tsv')
+## total
+#sum([aaa.numof_records() for aaa in a.annots.values()]) # 2191042
