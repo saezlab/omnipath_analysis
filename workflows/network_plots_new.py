28475677c0a4e567bdead790c03bfd75c371224b
Türei Dénes <turei.denes@gmail.com>
2019-10-25 00:13:43 +0200

    counts base: horizonatl bar instead of scatter, split by categories

--- workflows/network_plots_new.py
+++ workflows/network_plots_new.py
@@ -19,6 +19,9 @@
 #
 
 import imp
+import collections
+
+import numpy as np
 
 from pypath import session_mod
 
@@ -65,15 +68,16 @@ class CountsScatterBase(plot.PlotBase):
                 (
                     'by_resource',
                     'shared',
-                    'shared_cat',
+                    'shared_res_cat',
                 ),
                 (
                     'by_resource',
                     'shared',
-                    'shared_cat',
+                    'shared_res_cat',
                 ),
             ),
             by_category = True,
+            split_by_categories = True,
             order_by = ('entities', 'by_resource'),
             palette = None,
             **kwargs
@@ -84,6 +88,7 @@ class CountsScatterBase(plot.PlotBase):
         self.variables = variables
         self.variables2 = variables2
         self.by_category = by_category
+        self.split_by_categories = split_by_categories
         self.order_by = order_by
         self.palette = palette
         
@@ -93,7 +98,7 @@ class CountsScatterBase(plot.PlotBase):
             'fname': 'netw_node_edge_counts_pdf',
             'fname_param': (self.network_dataset,),
             'ylab': 'Resources',
-            'height': 7,
+            'height': 9,
             'width': 3 * cols,
             'grid_cols': cols,
             'legend': False,
@@ -124,7 +129,7 @@ class CountsScatterBase(plot.PlotBase):
         
         self.main_var = getattr(self, self.order_by[0])
         
-        self.labels = [
+        self.labels = np.array([
             it[0] for it in
             sorted(
                 getattr(
@@ -143,56 +148,131 @@ class CountsScatterBase(plot.PlotBase):
                         ()
                     ) +
                     (it[1],)
+                        if it[0] != 'Total' else
+                    ('ZZZ', 99999)
                 )
             )
-        ]
+        ])
+        
+        if self.split_by_categories:
+            
+            self.cat_split = np.array([
+                'Total'
+                    if lab == 'Total' else
+                self.main_var.counts.resource_cat[lab]
+                for lab in self.labels
+            ])
         
         self.tick_loc = range(len(self.labels))
         
         self.values = [
             [
-                [
+                np.array([
                     getattr(
                         getattr(self, var1).counts,
                         var2,
                     )[lab]
                     for lab in self.labels
-                ]
+                ])
                 for var2 in self.variables2[ivar]
             ]
             for ivar, var1 in enumerate(self.variables)
         ]
         
         self.colors = [
-            [
+            np.array([
                 workflows.colors.lightness(
                     self.palette.colors[ivar],
                     self.lightness_steps[ivar2],
                 )
                 for ivar2 in range(len(self.variables2[ivar]))
-            ]
+            ])
             for ivar, var1 in enumerate(self.variables)
         ]
+        
+        if self.split_by_categories:
+            
+            self.grid_rows = len(set(self.cat_split))
+            cat_counts = collections.Counter(self.cat_split)
+            self.categories = sorted(
+                cat_counts.keys(),
+                key = lambda lab: 'ZZZ' if lab == 'Total' else lab
+            )
+            self.grid_hratios = [cat_counts[cat] for cat in self.categories]
     
     
     def make_plots(self):
         
-        for ivar1, var1 in enumerate(self.variables):
+        for icat, cat in enumerate(
+            self.categories
+                if self.split_by_categories else
+            ('Resources',)
+        ):
             
-            self.get_subplot(0, ivar1)
-            
-            for ivar2 in reversed(range(len(self.variables2[ivar1]))):
+            for ivar1, var1 in enumerate(self.variables):
                 
-                self.ax.scatter(
-                    self.values[ivar1][ivar2],
-                    self.tick_loc,
-                    color = self.colors[ivar1][ivar2],
-                    facecolors = 'none',
-                )
-            
-            self.ax.set_yticks(self.tick_loc)
-            self.ax.set_yticklabels(self.labels)
-            self.ax.grid()
-            #self.ax.legend(loc = 0)
-            self.ax.set_ylim(-1, len(self.tick_loc))
-            self.ax.set_axisbelow(True)
+                self.get_subplot(icat, ivar1)
+                
+                for ivar2 in range(len(self.variables2[ivar1])):
+                    
+                    tick_loc = (
+                        range((self.cat_split == cat).sum())
+                            if self.split_by_categories else
+                        self.tick_loc
+                    )
+                    
+                    self.ax.barh(
+                        tick_loc,
+                        self.values[ivar1][ivar2][
+                            self.cat_split == cat
+                                if self.split_by_categories else
+                            np.array([True] * len(tick_loc))
+                        ],
+                        color = self.colors[ivar1][ivar2],
+                        #facecolors = 'none',
+                    )
+                
+                self.ax.set_yticks(tick_loc)
+                
+                if ivar1 == 0:
+                    
+                    self.ax.set_yticklabels(
+                        self.labels[
+                            self.cat_split == cat
+                                if self.split_by_categories else
+                            np.array([True] * len(tick_loc))
+                        ]
+                    )
+                    
+                else:
+                    
+                    self.ax.set_yticklabels([])
+                
+                self.ax.tick_params(axis = 'both', length = 0)
+                self.ax.locator_params(nbins = 6, axis = 'x')
+                
+                self.ax.grid()
+                #self.ax.legend(loc = 0)
+                self.ax.set_ylim(-1, len(tick_loc))
+                self.ax.set_axisbelow(True)
+                
+                self.post_subplot_hook()
+                
+                if ivar1 == 0:
+                    
+                    self.ax.set_ylabel(cat)
+                    
+                else:
+                    
+                    self.ax.get_yaxis().label.set_visible(False)
+                
+                if icat != self.grid_rows - 1:
+                    
+                    self.ax.set_xticklabels([])
+                    self.ax.get_xaxis().label.set_visible(False)
+                    
+                else:
+                    
+                    self.ax.set_xlabel(
+                        'Number of %s' % var1.replace('_', ' ')
+                    )
