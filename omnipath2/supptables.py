06c89f69ac23729c537d177b0a5f6bae782e7c65
Türei Dénes <turei.denes@gmail.com>
2019-11-07 12:06:17 +0100

    new module: supplementary tables

---
+++ omnipath2/supptables.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+# Copyright 2019 Saez Lab
+#
+# OmniPath2 analysis and figures suit
+#
+# Authors:
+#
+# Nicolàs Palacio-Escat
+# nicolas.palacio@bioquant.uni-heidelberg.de
+#
+# Dénes Türei
+# turei.denes@gmail.com
+#
+
