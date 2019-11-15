da1e4a40a492a4b313b3837e1e4a63a813d66a13
Türei Dénes <turei.denes@gmail.com>
2019-11-15 14:40:02 +0100

    R: workflow module

---
+++ R/workflow.r
@@ -0,0 +1,29 @@
+#!/usr/bin/env Rscript
+
+#
+#  This file is part of the `omnipath2` R package
+#
+#  Copyright
+#  2019
+#  Heidelberg University, Uniklinik RWTH Aachen
+#
+#  File author(s): Dénes Türei (turei.denes@gmail.com)
+#                  Nicolàs Palacio
+#
+#  Distributed under the GPLv3 License.
+#  See accompanying file LICENSE.txt or copy at
+#      http://www.gnu.org/licenses/gpl-3.0.html
+#
+#  Website: http://pypath.omnipathdb.org/
+#
+
+require(R6)
+
+
+omnipath2_workflow <- list(
+    test = Task$new(
+        method = ResourceByEntity,
+        name = NULL,
+        Param$new(list('omnipath', 'curated'))
+    )
+)
