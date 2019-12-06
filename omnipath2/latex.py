eceda23877b2bffe760ac7e57bbb6435c8398711
Türei Dénes <turei.denes@gmail.com>
2019-12-06 11:03:42 +0100

    new module: latex

---
+++ omnipath2/latex.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+# Copyright 2019 Saez Lab
+#
+# OmniPath2 analysis and figures suite
+#
+# Authors:
+#
+# Nicolàs Palacio-Escat
+# nicolas.palacio@bioquant.uni-heidelberg.de
+#
+# Dénes Türei
+# turei.denes@gmail.com
+#
+#
+#  Distributed under the GPLv3 License.
+#  See accompanying file LICENSE.txt or copy at
+#      http://www.gnu.org/licenses/gpl-3.0.html
+#
+#  Website: http://omnipathdb.org/
+#
+
