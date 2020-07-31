8ae5cf53c0a4e650a84845c952201fb29fbbdbb1
Türei Dénes <turei.denes@gmail.com>
2020-07-31 02:58:21 +0200

    building the databases without KEGG for the paper

--- main.py
+++ main.py
@@ -22,7 +22,28 @@
 #  Website: http://omnipathdb.org/
 #
 
+import importlib as imp
+
 import omnipath2.main
+from omnipath2 import settings
+from pypath.resources import data_formats
+from pypath.resources import network as netres
+from pypath.core import annot
+
+def kegg_off():
+
+    settings.setup(tf_target_pickle = 'tftarget_paper.pickle')
+    settings.setup(omnipath_pickle = 'omnipath_paper.pickle')
+    settings.setup(annotations_pickle = 'annotations_paper.pickle')
+    del data_formats.pathway_noref['kegg']
+    del data_formats.transcription_onebyone['kegg']
+    del data_formats.transcription['kegg']
+    annot.protein_sources_default.discard('KEGG')
+    imp.reload(netres)
+
+
+
+kegg_off()
 
 workflow = omnipath2.main.Main()
 
